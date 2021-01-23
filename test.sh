139b5a555946b46f3d41cba6d1e6e91067db61ae
Miki Shapiro <mikishapiro@gmail.com>
2021-01-23 14:53:44 +1100

    Renamed role and added molecule tests and lint

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sed -i 's@fedora_url=.*@fedora_url=https://download.fedoraproject.org/pub/fedora/linux/releases/33/Server/aarch64/images/Fedora-Server-33-1.3.aarch64.raw.xz@' ./inventory
-sed -i 's@fedora_image_checksum=.*@fedora_image_checksum=https://mirror.aarnet.edu.au/pub/fedora/linux/releases/33/Server/aarch64/images/Fedora-Server-33-1.3-aarch64-CHECKSUM@' ./inventory
-time ansible-playbook -i inventory ./deploy.yml || exit 133
-time ansible-playbook -i inventory ./deploy.yml || exit 233
-sed -i 's@fedora_url=.*@fedora_url=https://mirror.aarnet.edu.au/pub/fedora/linux/releases/32/Server/aarch64/images/Fedora-Server-32-1.6.aarch64.raw.xz@' ./inventory
-sed -i 's@fedora_image_checksum=.*@fedora_image_checksum=https://mirror.aarnet.edu.au/pub/fedora/linux/releases/32/Server/aarch64/images/Fedora-Server-32-1.6-aarch64-CHECKSUM@' ./inventory
-time ansible-playbook -i inventory ./deploy.yml || exit 132
-time ansible-playbook -i inventory ./deploy.yml || exit 232
+pip3 install molecule
+cd roles/fedora2sd 
+molecule --verbose converge
